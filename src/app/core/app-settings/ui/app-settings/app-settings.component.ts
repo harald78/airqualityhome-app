2d2e1451eb3e4e26da947398bb2cd67105e9dcff
haraldh <harald.hassler@lht.dlh.de>
2024-06-10 08:56:34 +0200

    DWS-71: Initial commit

---
+++ src/app/core/app-settings/ui/app-settings/app-settings.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-settings',
+  standalone: true,
+  imports: [],
+  templateUrl: './app-settings.component.html',
+  styleUrl: './app-settings.component.scss'
+})
+export class AppSettingsComponent {
+
+}
