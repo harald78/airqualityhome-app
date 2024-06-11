f07816cb839edaab89c8859925e46099e92ee99f
haraldh <harald.hassler@lht.dlh.de>
2024-06-11 20:51:40 +0200

    Added chart-component to capsulate ngx-chart into an ui-component

--- src/app/features/dashboard/ui/dashboard-detail/dashboard-detail.component.ts
+++ src/app/features/dashboard/ui/dashboard-detail/dashboard-detail.component.ts
@@ -1,58 +1,30 @@
 import {Component, inject, OnInit} from '@angular/core';
 import {MeasurementService} from "../../service/measurement.service";
-import {MeasurementHistory, SensorMeasurementHistory} from "../../model/measurementHistory.model";
-import {colorSets, LegendPosition, NgxChartsModule} from "@swimlane/ngx-charts";
+import {MeasurementHistory} from "../../model/measurementHistory.model";
 import {ActivatedRoute} from "@angular/router";
 import {MeasurementState} from "../../+state/measurement.state";
 import {LatestMeasurement} from "../../model/measurement.model";
 import {mdiFilter} from "@mdi/js";
 import {IconButtonComponent} from "../../../../shared/components/icon-button/icon-button.component";
 import { Router } from '@angular/router';
-
-import {ChartService} from "../../service/chart.service";
+import { ChartComponent } from './chart/chart.component';
 
 @Component({
   selector: 'app-dashboard-detail',
   standalone: true,
-  imports: [NgxChartsModule, IconButtonComponent],
+  imports: [IconButtonComponent, ChartComponent],
   templateUrl: './dashboard-detail.component.html',
   styleUrl: './dashboard-detail.component.scss'
 })
 export class DashboardDetailComponent implements OnInit {
-  public historyData: MeasurementHistory;
-  public chartData: SensorMeasurementHistory[];
-  public measurementItemName: string = '';
-
-  // view: [number, number] = [370, 300];
-  showXAxis = true;
-  showYAxis = true;
-  gradient = false;
-  showLegend = true;
-  showXAxisLabel = false;
-  showYAxisLabel = false;
-  yScaleMin = 0;
-  yScaleMax = 40;
-  roundDomains = true;
-  autoScale = false;
-  legendPosition: LegendPosition = LegendPosition.Below
-  colorScheme = {
-    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
-  };
-  referenceLines = [
-    {name: "maxAlarm", value: 30.0},
-    {name: "minAlarm", value: 18.0},
-  ]
 
   private readonly measurementService: MeasurementService = inject(MeasurementService);
   private readonly route = inject(ActivatedRoute);
   private readonly measurementState = inject(MeasurementState);
-  private readonly chartService: ChartService = inject(ChartService);
   private readonly router = inject(Router);
-  public latestMeasurement: LatestMeasurement | undefined;
 
-  constructor() {
-    this.colorScheme = colorSets.find(s => s.name === 'vivid')!;
-  }
+  public latestMeasurement: LatestMeasurement | undefined;
+  public historyData: MeasurementHistory;
 
   async ngOnInit() {
     const id = this.route.snapshot.params['id'];
@@ -60,7 +32,6 @@ export class DashboardDetailComponent implements OnInit {
     this.latestMeasurement = this.measurementState.filteredBySelection();
     if (this.latestMeasurement) {
       this.historyData = await this.measurementService.getMeasurementHistory(this.latestMeasurement.sensorId);
-      this.prepareChartData();
     } else {
       //Toast routes to Dashboard
     }
@@ -68,15 +39,6 @@ export class DashboardDetailComponent implements OnInit {
 
   async navigateBack() {
     await this.router.navigate(['dashboard']);
-  }
-
-  prepareChartData() {
-    if (this.historyData) {
-      this.yScaleMax = this.chartService.calculateYScaleMax(this.historyData.data);
-      this.yScaleMin = this.chartService.calculateYScaleMin(this.historyData.data);
-      this.referenceLines = this.chartService.calculateReferenceLines(this.historyData.data);
-      this.chartData = this.historyData.data;
-    }
   }
 
   protected readonly mdiFilter = mdiFilter;
